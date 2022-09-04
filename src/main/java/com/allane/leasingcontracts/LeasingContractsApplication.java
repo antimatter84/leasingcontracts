40ee44ff08479cc248f7defd130545ba3346f010
Andreas <antimatter84@quantentunnel.de>
2022-09-04 13:52:44 +0200

    Initial commit

---
+++ src/main/java/com/allane/leasingcontracts/LeasingContractsApplication.java
@@ -0,0 +1,13 @@
+package com.allane.leasingcontracts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeasingContractsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeasingContractsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/allane/leasingcontracts/LeasingContractsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.allane.leasingcontracts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeasingContractsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
