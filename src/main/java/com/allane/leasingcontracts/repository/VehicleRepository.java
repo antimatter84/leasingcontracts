7d5d6ee622c410859573712e5010f57133f20d10
Andreas <antimatter84@quantentunnel.de>
2022-09-30 16:23:16 +0200

    Created endpoint to get unique vehicle brand names.

--- src/main/java/com/allane/leasingcontracts/repository/VehicleRepository.java
+++ src/main/java/com/allane/leasingcontracts/repository/VehicleRepository.java
@@ -2,8 +2,10 @@ package com.allane.leasingcontracts.repository;
 
 import com.allane.leasingcontracts.model.Vehicle;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -13,4 +15,7 @@ public interface VehicleRepository
     Optional<Vehicle> findVehicleByVin(String vin);
 
     boolean existsByIdIsNotAndVinEquals(Long id, String vin);
+
+    @Query("select distinct brand from Vehicle")
+    List<String> findDistinctBrands();
 }
